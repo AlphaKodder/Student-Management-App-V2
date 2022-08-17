10489587fdaaae2f582b167b5337015a998d4c4e
Daniel Stanculescu <danystanculescu@gmail.com>
2022-08-17 09:02:53 +0300

    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,27 @@ import {
   Routes
 } from 'react-router-dom';
 
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 import React from 'react';
+import Students from './components/Main/Students/Students';
+import Welcome from './components/Main/Welcome/Welcome';
 import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-            <Route path="*" element={<Header/>}/>     
-            
+      <Header/>
+      <Main>
+        <Routes>       
+            <Route path="/" element={<Welcome/>} />
+            <Route path="/students" element = {<Students/>}/>
         </Routes>
+      </Main>
+        
+       <Footer/>
       </BrowserRouter>
     </div>
   );
